dcf1929b397199f38077e68ae4c5f508b49aff2d
santidmc127 <santiagoelias128@gmail.com>
2024-01-22 23:53:31 -0500

    .

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Client, EmbedBuilder } = require("discord.js");
+const client = new Client({ intents: [3276799] });
+const config = require("./config.json");
+
+client.once("ready", () => {
+  console.log("Online Bot");
+});
+
+client.on("messageCreate", async (message) => {
+  const prefix = config.prefix;
+  if (!message.content.toLowerCase().startsWith(prefix)) return;
+});
+
+client.login(config.token);
